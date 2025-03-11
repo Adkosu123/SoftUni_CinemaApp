5905b93fce8772ea420f45fa3f97e8439a4f6ac0
Adrian Gudjev <adrian3829@abv.bg>
2025-03-11 21:14:12 +0200

    Connection string renamed

--- CinemaApp/Program.cs
+++ CinemaApp/Program.cs
@@ -11,7 +11,7 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        var connectionString = builder.Configuration.GetConnectionString("CinemaDbConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
         builder.Services.AddDbContext<CinemaDbContext>(options =>
             options.UseSqlServer(connectionString));
         builder.Services.AddDatabaseDeveloperPageExceptionFilter();
