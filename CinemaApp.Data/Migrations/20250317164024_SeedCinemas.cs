3e1ecff2264a13ecc9bae8a66712f20243490304
Adrian Gudjev <adrian3829@abv.bg>
2025-03-17 18:43:52 +0200

    Add seeding of Cinemas using Fluent API

---
+++ CinemaApp.Data/Migrations/20250317164024_SeedCinemas.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CinemaApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCinemas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Tickets",
+                type: "decimal(18,2)",
+                nullable: false,
+                comment: "Ticket Price",
+                oldClrType: typeof(decimal),
+                oldType: "money(18, 2)",
+                oldComment: "Ticket Price");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+
+            migrationBuilder.InsertData(
+                table: "Cinemas",
+                columns: new[] { "Id", "Location", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("78ff97e2-cb87-40a5-9a9c-cb5b83cd0db6"), "Sofia", "Cinema city" },
+                    { new Guid("ef42c3dd-5406-4a1c-84c2-c681eff017fc"), "Plovdiv", "Cinema city" },
+                    { new Guid("fafc9e00-03a1-4fbb-a805-455688c051c0"), "Varna", "Cinemax" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Cinemas",
+                keyColumn: "Id",
+                keyValue: new Guid("78ff97e2-cb87-40a5-9a9c-cb5b83cd0db6"));
+
+            migrationBuilder.DeleteData(
+                table: "Cinemas",
+                keyColumn: "Id",
+                keyValue: new Guid("ef42c3dd-5406-4a1c-84c2-c681eff017fc"));
+
+            migrationBuilder.DeleteData(
+                table: "Cinemas",
+                keyColumn: "Id",
+                keyValue: new Guid("fafc9e00-03a1-4fbb-a805-455688c051c0"));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Tickets",
+                type: "money(18, 2)",
+                nullable: false,
+                comment: "Ticket Price",
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldComment: "Ticket Price");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserTokens",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProviderKey",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LoginProvider",
+                table: "AspNetUserLogins",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
